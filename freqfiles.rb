e65e31c79935f1e320bc68d54bd9e60bd42e3605
masao <tmasao@acm.org>
2010-01-23 09:23:39 +0000

    SYMBOL_REGEXP

--- freqfiles.rb
+++ freqfiles.rb
@@ -13,6 +13,9 @@ module Math
    end
 end
 
+# ASCII and Kanji symbol chars
+SYMBOL_REGEXP =  /\A[\x00-\x2f\x3a-\x40\x5b-\x60\x7b-\x7f、。【】「」〜～・□■◇◆☆▽▲△]+\Z/o
+
 def extract_keywords_mecab( str, method = :default )
    mecab = MeCab::Tagger.new( '--node-format=%m\t%H\t%c\n --unk-format=%m\tUNK\t%c\n' )
    str = NKF.nkf( "-Wem0XZ1", str ).gsub( /\s+/, " " ).strip
@@ -25,13 +28,14 @@ def extract_keywords_mecab( str, method = :default )
    lines.each_with_index do |l, i|
       next if not l[2]
       next if not l[1] =~ /^名詞|UNK|形容詞/o
-      next if l[0] =~ /\A[\x00-\x2f\x3a-\x40\x5b-\x60\x7b-\x7f]+\Z/o # ASCII symbol chars
+      next if l[1] =~ /接尾/
+      next if l[0] =~ SYMBOL_REGEXP
       j = 1
       while lines[ i+j ]
          break if j > 6
          break if not lines[i+j][2]
          break if not lines[i+j][1] =~ /^名詞|UNK|形容詞/o
-         break if lines[i+j][0] =~ /\A[\x00-\x2f\x3a-\x40\x5b-\x60\x7b-\x7f]+\Z/o # ASCII symbol chars
+         break if lines[i+j][0] =~ SYMBOL_REGEXP
          lines_composite << [ lines[i,j+1].map{|e| e[0] }.join,
                               "複合語-" + lines[i,j+1].map{|e| e[1] }.join("|"),
                               lines[i,j+1].inject(0){|sum,num| sum+=num[2].to_i },
@@ -48,7 +52,7 @@ def extract_keywords_mecab( str, method = :default )
    count = Hash.new( 0 )
    score = 0
    lines_ind.each_with_index do |line, idx|
-      next if line[0] =~ /\A[\x00-\x2f\x3a-\x40\x5b-\x60\x7b-\x7f]+\Z/o # ASCII symbol chars
+      next if line[0] =~ SYMBOL_REGEXP
       next if line[0] =~ /\A(?:w(?:h(?:e(?:re(?:a(?:[st]|fter)|u(?:nto|pon)|in(?:to)?|o[fn]?|from|with|ver|by)?|n(?:(?:so)?ever|ce)?|ther)|o(?:m(?:(?:so)?ever)?|s(?:oever|e)|ever|le)?|i(?:ch(?:(?:so)?ever)?|l(?:st|e)|ther)|at(?:(?:so)?ever)?|y)|i(?:th(?:out|in)?|ll)|e(?:ll|re)?|ould|as)|a(?:l(?:(?:bei|mos)t|on[eg]|though|ready|ways|so|l)|n(?:y(?:(?:wher|on)e|thing|how)?|other|d)?|fter(?:wards)?|bo(?:ut|ve)|gain(?:st)?|mong(?:st)?|r(?:ound|e)|(?:cros)?s|dj|t)?|t(?:h(?:e(?:re(?:(?:upo|i)n|afte|for|by)?|m(?:selves)?|n(?:ce)?|ir|se|y)?|r(?:ough(?:out)?|u)|o(?:ugh|se)|[iu]s|a[nt])|o(?:gether|wards?|o)?)?|s(?:o(?:me(?:t(?:imes?|hing)|(?:wher|on)e|how)?)?|e(?:em(?:ing|ed|s)?|veral)|(?:inc|am)e|h(?:ould|e)|till|uch)?|b(?:e(?:c(?:om(?:es?|ing)|a(?:us|m)e)|fore(?:hand)?|(?:hi|yo)nd|(?:twe)?en|sides?|ing|low)?|oth|ut|y)|h(?:e(?:r(?:e(?:(?:upo|i)n|by)?|s(?:elf)?)?|eafter|nce)?|i(?:m(?:self)?|s)|a(?:[ds]|ve)|ow(?:ever)?)|o(?:u(?:r(?:(?:selve)?s)?|t)|n(?:ce one|ly|to)?|ther(?:wise|s)?|f(?:ten|f)?|(?:ve)?r|wn)|e(?:ve(?:r(?:y(?:(?:wher|on)e|thing)?)?|n)|ls(?:ewher)?e|(?:noug|ac)h|ither|xcept|tc|g)|n(?:o(?:[rw]|t(?:hing)?|body|o?ne)?|e(?:ver(?:theless)?|ither|xt)|amely|where)|m(?:o(?:re(?:over)?|st(?:ly)?)|(?:eanwhil)?e|u(?:ch|st)|y(?:self)?|an?y|ight)|i(?:[efs]|n(?:deed|to|c)?|t(?:s(?:elf)?)?)?|f(?:or(?:mer(?:ly)?)?|urther|irst|rom|ew)|l(?:a(?:tter(?:ly)?|st)|e(?:ast|ss)|td)|y(?:ou(?:r(?:s(?:el(?:ves|f))?)?)?|et)|x(?:author|other |note|subj|cal)|u(?:n(?:der|til)|p(?:on)?|s)|c(?:an(?:not)?|o(?:uld)?)|d(?:uring|own)|per(?:haps)?|v(?:ery|ia)|rather)\Z/o
       #next if line[0].size < 3
       #p line[2]
