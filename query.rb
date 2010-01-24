aeda4cec9080ae05d976aedd9d44315f3dc14e7f
masao <tmasao@acm.org>
2010-01-24 11:15:04 +0000

    アドホック版（訃報処理…）はコメントアウトしておく

--- query.rb
+++ query.rb
@@ -1,4 +1,5 @@
 #!/usr/bin/env ruby
+# -*- coding: utf-8 -*-
 # $Id$
 
 require "rexml/document"
@@ -6,25 +7,16 @@ require "io/nonblock"
 
 require "freqfiles.rb"
 
-# $stdout.nonblock = true
-
 topics = REXML::Document.new( open "GeoTime-EN-JA-Topics_ntcirEdited_10.xml" )
 topics.elements.each( '//TOPIC' ) do |t|
    STDERR.puts "### #{ t.attributes[ 'ID' ] }"
    d = t.elements.each( './DESCRIPTION' ) do |d|
       next if not d.attributes['LANG'] == "JA"
       text = d.text.sub( /\A<!\[CDATA\[/, "" ).sub( /\]\]>\Z/, "" )
+      # text << " 訃報 / 死去" if text =~ /亡くな/
       q = extract_keywords_mecab( text )
       open( "| ./search.pl geotime", "w" ) do |io|
          io.puts q.map{|e| e[0] }
       end
    end
 end
-
-# topics.each do |q|
-#    res_file = ".GC-#{q.num}.res"
-#    next if File.exist?(res_file)
-#    open("|./jma.pl | time ./search.pl -n 1000 -d #{ARGV.join(" ")}", "w") do |f|
-#       f.puts q.title.split(/,/).join(" ")
-#    end
-# end
