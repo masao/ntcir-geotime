454d152e650309123aa9f111176b7dd2fa45770f
masao <tmasao@acm.org>
2010-01-25 04:51:06 +0000

    changed for geotime

--- format_res.pl
+++ format_res.pl
@@ -9,10 +9,10 @@ my $runid = shift @ARGV;
 my $num = 0;
 my $qid = undef;
 while (<>) {
-    if (/^###\s*([A-Z\d]+)$/) {
+    if (/^###\s*([A-Za-z\d\-]+)$/) {
 	$qid = $1;
 	$num = 0;
-    } elsif (/^(\d{7}_\d{7})\t([\d\.e\-]+)$/) {
+    } elsif (/^([A-Z\d\-]+)\t([\d\.e\-]+)$/) {
 	print "$qid\t0\t$1\t0\t$2\t$runid\n" if $num < 100;
 	$num++;
     } else {
